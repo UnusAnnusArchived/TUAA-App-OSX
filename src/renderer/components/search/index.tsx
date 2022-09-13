e1f65982d16af86b1c692a8d61cde76077950358
ScribbleNerd <contact@scribblenerd.com>
2022-09-13 10:36:09 -0500

    Add search bar & search bar styling

--- src/renderer/components/search/index.tsx
+++ src/renderer/components/search/index.tsx
@@ -1,10 +1,19 @@
+import { useRef } from 'react';
 import styles from './styles.module.scss';
 
 const SearchBar: React.FC = () => {
+  const searchBar = useRef<HTMLInputElement>(null);
+
   return (
     <div className={styles.container}>
       <div className={styles.searchbox}>
-        <span>search bar</span>
+        <span className={styles.searchIcon}>􀊫</span>
+        <input
+          className={styles.searchBar}
+          ref={searchBar}
+          type="text"
+          placeholder="Search"
+        />
       </div>
     </div>
   );
